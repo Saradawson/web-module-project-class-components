b287429ca2e59b431d522f3e18965f079822222a
unknown <saradawsone@gmail.com>
2022-09-07 16:09:33 -0700

    built out

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,18 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <ul>
+          <li>Clean Room</li>
+          <li>Feed Cat</li>
+          <li>Make Dinner</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
